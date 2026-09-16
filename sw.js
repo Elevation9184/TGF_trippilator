d9d266b52fd7386532878f49ddde5dc65d450c66
Elevation9184 <rob.goudie@gmail.com>
2026-09-16 14:54:47 +1200

    My day follows the day: heading to, here, and ticked off as you go

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  * Bump CACHE when publishing a new bundle. Old caches are removed on activate.
  */
 
-const CACHE = "tgo-20260916-1441";
+const CACHE = "tgo-20260916-1454";
 const ASSETS = [
   "./",
   "./index.html",
